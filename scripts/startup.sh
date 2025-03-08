0d7e7440c5ee847c6325cfeb966c61486dbab68d
Caique S. Soares <caiqueoucaique@hotmail.com>
2025-03-08 15:43:28 -0300

    Update startup.sh

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -35,8 +35,8 @@ aws --version
 echo "Instalando Terraform..."
 # Baixa a versão mais recente do Terraform
 apt update -y
-pt install gpg
-pt install wget
+apt install gpg
+apt install wget
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(grep -oP '(?<=UBUNTU_CODENAME=).*' /etc/os-release || lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
